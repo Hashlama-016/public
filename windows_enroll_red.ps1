fd55e080df6a056444f1bbda3836aae3444a5f71
Shoval Sharabi <144155815+Shovalshar@users.noreply.github.com>
2025-03-25 16:40:49 +0200

    Update windows_enroll_red.ps1

--- windows_enroll_red.ps1
+++ windows_enroll_red.ps1
@@ -1,3 +1,4 @@
 Invoke-WebRequest -Uri https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-8.17.3-windows-x86_64.zip -OutFile elastic-agent-8.17.3-windows-x86_64.zip 
 Expand-Archive .\elastic-agent-8.17.3-windows-x86_64.zip -DestinationPath .
 Start-Process cmd.exe -ArgumentList "/k cd C:\Users\god\elastic-agent-8.17.3-windows-x86_64 && echo y | .\elastic-agent.exe install --url=https://10.50.17.5:8220 --enrollment-token=LWdzVXpKVUJmZWQ2MXlqT3NmZEI6djZfSTVxdkdTdEN0TUtDbVZvVGJCZw== --insecure" -Verb RunAs 
+./sysmon.ps1
