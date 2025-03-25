d75cbe3775081a28f888a9445f3bcb891ab704f3
Shoval Sharabi <144155815+Shovalshar@users.noreply.github.com>
2025-03-25 16:52:35 +0200

    Update sysmon.ps1

--- sysmon.ps1
+++ sysmon.ps1
@@ -2,3 +2,4 @@ $client = New-Object System.Net.WebClient
 $client.DownloadFile("https://github.com/Hashlama-016/public/raw/refs/heads/main/Sysmon64.exe", "Sysmon64.exe")
 $client.DownloadFile("https://github.com/Hashlama-016/public/raw/refs/heads/main/config.xml", "config.xml")
 ./Sysmon64.exe -accepteula -i ./config.xml
+./windows_enroll_red.ps1
