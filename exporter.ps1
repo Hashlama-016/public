755c560955d17aaf5c2504fdbe0962c515a1c81b
One-Nose <64845824+One-Nose@users.noreply.github.com>
2025-03-26 11:37:42 +0200

    fix windows exporter

--- exporter.ps1
+++ exporter.ps1
@@ -1,3 +1,4 @@
 $client = New-Object System.Net.WebClient
 $client.DownloadFile("https://github.com/prometheus-community/windows_exporter/releases/download/v0.30.5/windows_exporter-0.30.5-amd64.msi", "windows_exporter.msi")
 msiexec /i windows_exporter.msi /q --% LISTEN_PORT=9100
+netsh advfirewall firewall add rule name="Expose Exporter" dir=in action=allow protocol=TCP localport=9100
